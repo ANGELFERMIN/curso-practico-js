dcbf56fbe15a8c06ac048c8734178d04d1f5b09b
Angel Fermin <angelfermin14@gmail.com>
2022-08-20 17:39:44 -0400

    primer commit de 20 de agosto 2022 , se procese a midificar el HTML apartir de aqu

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ console.log({
     parafito,
     pid,
     input});
+//h1.innerHTML='Caballo <br> feo';
+console.log(h1.getAttribute('pantalla'));
+console.log(h1.getAttribute('class'));
+//console.log(h1.setAttribute('class', 'camote'));
+h1.classList.add('habichuela');
+//h1.classList.remove('camote');
+input.value=123;
+const img =document.createElement('img');
+img.setAttribute('src', 
+'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/01_%C2%BFQu%C3%A9-puedo-hacer-si-mi-gato-est%C3%A1-triste-.png?itok=w67Nhubc');
+console.log(img);
+pid.innerHTML="";
+pid.append(img);
+
+
+
+
+
