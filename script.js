fb95df09498830289bc1993a168c19441409c586
Angel Fermin <angelfermin14@gmail.com>
2022-08-20 23:05:21 -0400

    primer commit de 20 de agosto 2022 , probamos onclick desde nuesto codigo de HTML

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
-
-console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    parafito,
-    pid,
-    input});
-//h1.innerHTML='Caballo <br> feo';
-console.log(h1.getAttribute('pantalla'));
-console.log(h1.getAttribute('class'));
-//console.log(h1.setAttribute('class', 'camote'));
-h1.classList.add('habichuela');
-//h1.classList.remove('camote');
-input.value=123;
-const img =document.createElement('img');
-img.setAttribute('src', 
-'https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/01_%C2%BFQu%C3%A9-puedo-hacer-si-mi-gato-est%C3%A1-triste-.png?itok=w67Nhubc');
-console.log(img);
-pid.innerHTML="";
-pid.append(img);
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const resrl= document.querySelector('#resrl');
 
 
+function btnOnClick(){
+    let a= parseInt(input1.value);
+    let b= parseInt(input2.value);
+
+    console.log('mira click')
+
+    let c=a+b;
+    resrl.innerHTML=`Resultado: ${c}`;
 
 
+}
 
