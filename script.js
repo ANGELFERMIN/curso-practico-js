ce85a7554c524bd93f22bb0169dc39d25437398a
Angel Fermin <angelfermin14@gmail.com>
2022-08-21 00:02:43 -0400

    primer commit de 20 de agosto 2022 , se agrego el gitignore y cambios de la clase de eventos

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const resrl= document.querySelector('#resrl');
 
-
-function btnOnClick(){
+form.addEventListener('submit', sumarInputValues);
+function sumarInputValues(event){
+    event.preventDefault();
     let a= parseInt(input1.value);
     let b= parseInt(input2.value);
-
     console.log('mira click')
-
     let c=a+b;
     resrl.innerHTML=`Resultado: ${c}`;
-
-
 }
-
